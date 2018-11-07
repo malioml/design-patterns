a6f37f8d27b651b8651cc1640811892f2b9e7de5
root <root@vps322051.ovh.net>
2018-11-07 20:14:22 +0100

    handheldinventoryiterator

--- src/iterator/HandHeldInventoryIterator.ts
+++ src/iterator/HandHeldInventoryIterator.ts
@@ -1,3 +1,26 @@
-class HandHeldInventoryIterator {
-
+class HandHeldInventoryIterator implements IInventoryIterator {
+	inventory: HandHeldInventory
+	index: number = 0
+	
+	constructor(handHeldInventory: HandHeldInventory) {
+		this.inventory = handHeldInventory
+	}
+	
+	isDone(): boolean {
+		return this.index < 2
+	}
+	
+	next(): void {
+		this.index += 1
+	}
+	
+	current(): IItem {
+		switch (this.index) {
+			case 0 : return this.inventory.right
+			case 1 : return this.inventory.left
+			default : return null
+		}
+	}
 }
+
+export default HandHeldInventoryIterator
